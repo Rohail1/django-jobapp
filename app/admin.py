d2fd44e1fb1843fa95bd51c84fd8cacf70b8b1fd
Rohail Najam <rohail.najam@oxit.com>
2023-07-18 13:54:47 +0500

    Added Template inheritance

--- app/admin.py
+++ app/admin.py
@@ -8,7 +8,7 @@ class JobAdmin(admin.ModelAdmin):
     list_display = ('title', 'date', 'salary', 'location', 'author')
     list_filter = ('date', 'salary', 'expiry')
     search_fields = ('title', 'description')
-    search_help_text = 'Write in your query and hit enter'
+    search_help_text = 'Write in your  query and hit enter'
 
 
 admin.site.register(JobPost, JobAdmin)
